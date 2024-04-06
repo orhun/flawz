3fbea15879034310a08be5bc5e6e15d29ce6ff20
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2024-04-06 16:08:00 +0300

    feat: add basic CVE querying

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use nvd_cve::cache::{get_all, sync_blocking, CacheConfig};
+use nvd_cve::client::{BlockingHttpClient, ReqwestBlockingClient};
+
+pub fn main() {
+    let mut config = CacheConfig::new();
+    config.feeds = vec!["2024".to_string()];
+
+    let client = ReqwestBlockingClient::new(&config.url, None, None, None);
+
+    if let Err(error) = sync_blocking(&config, client) {
+        eprintln!("Fatal Error while syncing feeds: {:?}", error);
+        std::process::exit(1);
+    }
+
+    if let Ok(cves) = get_all(&config) {
+        for cve in cves {
+            println!("{:#?}", &cve);
+        }
+    }
 }
