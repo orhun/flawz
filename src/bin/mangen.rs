1722db0db7ed20f5ea8226acf6bd3847988ab2b4
Integral <integral@member.fsf.org>
2024-12-08 19:23:03 +0800

    refactor: replace format! with concat! for string literals

--- src/bin/mangen.rs
+++ src/bin/mangen.rs
@@ -19,7 +19,7 @@ const OUT_DIR_ENV: &str = "OUT_DIR";
 /// See <https://doc.rust-lang.org/cargo/reference/environment-variables.html>
 fn main() -> Result<()> {
     let out_dir = env::var(OUT_DIR_ENV).unwrap_or_else(|_| panic!("{OUT_DIR_ENV} is not set"));
-    let out_path = PathBuf::from(out_dir).join(format!("{}.1", env!("CARGO_PKG_NAME")));
+    let out_path = PathBuf::from(out_dir).join(concat!(env!("CARGO_PKG_NAME"), ".1"));
     let app = Args::command();
     let man = Man::new(app);
     let mut buffer = Vec::<u8>::new();
